ce4930550f4497968ff1850b3c0706b285b9d1a4
djsegal <djsegal@wisc.edu>
2015-07-26 17:07:57 -0400

    Add index page for viewing venues’ hours

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    @days = [ 'Thursday', 'Friday', 'Saturday' ]
+    @times = [ '12am', '6am', '12pm', '6pm' ]
+    @venues = Venue.all
+  end
 end
